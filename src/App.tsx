3731854bb4ce829f9f89729f2cd12b63eb6d2431
Elif Balci <mrselifbalci@gmail.com>
2024-01-15 01:52:40 +0000

    Add auth link

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Auth from './pages/Auth';
+import { Box } from '@mui/material';
 
 function App() {
 	return (
 		<>
 			<Router>
-				<Link to='/auth'>Signin</Link>
 				<Routes>
+				    <Route path='/auth' element={<Box><Link to='/auth'>Signin</Link></Box>} />
 					<Route path='/auth' element={<Auth />} />
 				</Routes>
 			</Router>
