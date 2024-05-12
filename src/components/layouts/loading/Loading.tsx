3a23d8c65a57de9b327c3ebac74bd1cf87afa255
mustafa balci <mbalci85@gmail.com>
2024-05-12 18:39:52 +0100

    update - loading screen

--- src/components/layouts/loading/Loading.tsx
+++ src/components/layouts/loading/Loading.tsx
@@ -1,40 +1,193 @@
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { AppBar, Box, Button, CircularProgress, IconButton, Toolbar, Typography } from '@mui/material';
 import theme from '../../../themes';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { OrganisationContext } from '../../../contexts/OrganisationContextProvider';
+import { UserAuthContext } from '../../../contexts/UserAuthContextProvider';
+import SidebarBtn from '../dashboardLayout/SidebarBtn';
+import { Mode, Roles } from '../../../interfaces/enums';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import {
+	AssignmentIndRounded,
+	CalendarMonth,
+	DarkMode,
+	Email,
+	Groups,
+	LibraryBooks,
+	LightMode,
+	PeopleAltOutlined,
+	QuizOutlined,
+	Settings,
+} from '@mui/icons-material';
 
 const Loading = () => {
 	const { organisation } = useContext(OrganisationContext);
+	const { user } = useContext(UserAuthContext);
+	const [mode, setMode] = useState<Mode>((localStorage.getItem('mode') as Mode) || Mode.LIGHT_MODE);
+
+	const currentPage = window.location.pathname.includes('admin')
+		? window.location.pathname.split('/')[2].charAt(0).toUpperCase() + window.location.pathname.split('/')[2].slice(1)
+		: window.location.pathname.split('/')[1].charAt(0).toUpperCase() + window.location.pathname.split('/')[1].slice(1);
+
+	const [selectedPage, setSelectedPage] = useState<string>(currentPage);
 	return (
-		<Box
-			sx={{
-				display: 'flex',
-				flexDirection: 'column',
-				justifyContent: 'center',
-				alignItems: 'center',
-				backgroundColor: theme.bgColor?.secondary,
-				height: '100vh',
-			}}>
-			<CircularProgress />
-			<Typography
+		<>
+			<Box
 				sx={{
-					margin: '2rem',
-					fontSize: '2rem',
-					fontFamily: 'Poppins',
-					fontWeight: 500,
-					color: '#01435A',
+					display: 'flex',
+					flexDirection: 'column',
+					minHeight: '110vh',
+					width: 'calc(100% - 10rem)',
+					marginLeft: '10rem',
+					position: 'absolute',
+					right: 0,
 				}}>
-				Loading...
-			</Typography>
-			<Typography
+				<AppBar position='sticky'>
+					<Toolbar
+						sx={{
+							display: 'flex',
+							justifyContent: 'flex-end',
+							alignItems: 'center',
+							height: '3rem',
+							width: '100%',
+							backgroundColor: theme.textColor?.secondary.main,
+							padding: '0 1rem 0 3rem',
+						}}>
+						<Box>
+							{
+								{
+									[Mode.DARK_MODE]: (
+										<IconButton sx={{ color: theme.textColor?.common.main }}>
+											<DarkMode />
+										</IconButton>
+									),
+									[Mode.LIGHT_MODE]: (
+										<IconButton sx={{ color: theme.textColor?.common.main }}>
+											<LightMode />
+										</IconButton>
+									),
+								}[mode]
+							}
+							<Button
+								sx={{
+									textTransform: 'capitalize',
+									color: theme.textColor?.common.main,
+									fontFamily: theme.fontFamily?.main,
+								}}>
+								Log Out
+							</Button>
+						</Box>
+					</Toolbar>
+				</AppBar>
+			</Box>
+
+			<Box
 				sx={{
-					fontSize: '4rem',
-					fontFamily: 'Permanent Marker, cursive',
-					color: '#01435A',
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'flex-start',
+					alignItems: 'center',
+					width: '10rem',
+					minHeight: '100vh',
+					backgroundColor: theme.palette.primary.main,
+					position: 'fixed',
+					left: 0,
+					zIndex: 10,
 				}}>
-				{organisation?.orgName}
-			</Typography>
-		</Box>
+				<Box
+					sx={{
+						display: 'flex',
+						alignItems: 'center',
+						height: '3rem',
+						marginBottom: '1rem',
+					}}>
+					<Typography variant='h1' sx={{ color: theme.textColor?.common.main, fontSize: '1.75rem' }}>
+						{organisation?.orgName}
+					</Typography>
+				</Box>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						marginBottom: '1rem',
+					}}>
+					<img
+						src={user?.imageUrl}
+						alt='user_profile_pic'
+						style={{
+							height: '6rem',
+							width: '6rem',
+							borderRadius: '50%',
+							marginBottom: '0.5rem',
+						}}
+					/>
+					<Typography variant='body1' sx={{ color: theme.textColor?.common.main }}>
+						{user?.username}
+					</Typography>
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'flex-start',
+							alignItems: 'flex-start',
+							marginTop: '2rem',
+						}}>
+						{user?.role === Roles.ADMIN && (
+							<>
+								<SidebarBtn btnText='Dashboard' IconName={DashboardIcon} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Users' IconName={PeopleAltOutlined} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Courses' IconName={LibraryBooks} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Lessons' IconName={AssignmentIndRounded} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Questions' IconName={QuizOutlined} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Schedule' IconName={CalendarMonth} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Messages' IconName={Email} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Community' IconName={Groups} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Settings' IconName={Settings} selectedPage={selectedPage} />
+							</>
+						)}
+						{user?.role === Roles.USER && (
+							<>
+								<SidebarBtn btnText='Dashboard' IconName={DashboardIcon} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Courses' IconName={LibraryBooks} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Schedule' IconName={CalendarMonth} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Messages' IconName={Email} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Community' IconName={Groups} selectedPage={selectedPage} />
+								<SidebarBtn btnText='Settings' IconName={Settings} selectedPage={selectedPage} />
+							</>
+						)}
+					</Box>
+				</Box>
+			</Box>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: theme.bgColor?.secondary,
+					height: '110vh',
+				}}>
+				<CircularProgress />
+				<Typography
+					sx={{
+						margin: '2rem',
+						fontSize: '2rem',
+						fontFamily: 'Poppins',
+						fontWeight: 500,
+						color: '#01435A',
+					}}>
+					Loading...
+				</Typography>
+				<Typography
+					sx={{
+						fontSize: '4rem',
+						fontFamily: 'Permanent Marker, cursive',
+						color: '#01435A',
+					}}>
+					{organisation?.orgName}
+				</Typography>
+			</Box>
+		</>
 	);
 };
 
