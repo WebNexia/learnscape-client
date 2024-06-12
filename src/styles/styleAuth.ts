c3f62d736dc41dfd9cffbe21d5a627a68ee2752c
mustafa balci <mbalci85@gmail.com>
2024-06-12 06:17:42 +0100

    update - login design

--- src/styles/styleAuth.ts
+++ src/styles/styleAuth.ts
@@ -1,8 +1,8 @@
 export const formContainerStyles = () => ({
 	position: 'relative',
-	width: '50%',
-	height: '63vh',
-	padding: '6rem 3rem ',
+	width: '45%',
+	height: 'fit-content',
+	padding: '6rem 3rem 4rem 3rem',
 	border: '#01435A solid 0.1rem',
 	borderRadius: '0.2rem',
 	boxShadow: '0.1rem 0.1rem 0.2rem 0.2rem rgba(0,0,0,0.3)',
