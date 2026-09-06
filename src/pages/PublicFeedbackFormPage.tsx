8a1feebf0e210ad7d081a27897e80808c0d5da92
mustafa balci <mbalci85@gmail.com>
2026-09-06 22:55:27 +0100

    update forms

--- src/pages/PublicFeedbackFormPage.tsx
+++ src/pages/PublicFeedbackFormPage.tsx
@@ -1213,7 +1213,7 @@ const PublicFeedbackFormPage = () => {
 							sx={{
 								color: theme.textColor?.secondary.main,
 								mb: 1.5,
-								textAlign: 'center',
+								textAlign: 'left',
 								fontSize: '0.8125rem',
 								lineHeight: 1.5,
 								whiteSpace: 'pre-wrap',
